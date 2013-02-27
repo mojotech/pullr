0a311dce5f6f0d555562a895d958e6d29ec545d5
Sam Saccone <samccone@gmail.com>
2013-02-26 23:00:54 -0500

    [wip]
    todo: save local login info

--- bin/pullr.js
+++ bin/pullr.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 var colors      = require('colors');
 var credentials = require('../lib/credentials');
+var pullRequest = require('../lib/pull-request');
 
 credentials(function(err, token) {
   if (err) {
     console.log("Error".inverse.red + " " + JSON.stringify(err).yellow);
   } else {
-    console.log("Logged in!".green);
+    pullRequest(token);
   }
 });
